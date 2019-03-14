a96dac2d369a24dadeafbf9649131e3f7b3da27c
MagnusBrzenk <magnusbrzenk@gmail.com@gmail.com>
2019-03-14 00:25:44 -0400

    Init commit

---
+++ phpImageGallery.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="node_modules/lightgallery/dist/css/lightgallery.css" />
+    </head>
+    <body>
+
+        <!-- jQuery version must be >= 1.8.0; -->
+        <script src="node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="js/lightgallery.min.js"></script>
+
+        <!-- A jQuery plugin that adds cross-browser mouse wheel support. (Optional) -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
+
+        <!-- lightgallery plugins -->
+        <script src="node_modules/lightgallery/modules/lg-thumbnail.min.js"></script>
+        <script src="node_modules/lightgallery/modules/lg-fullscreen.min.js"></script>
+
+        <?php
+
+function scd($dir)
+{
+    $files = scandir($dir);
+    sort($files);
+    reset($files);return $files;
+}
+
+$output = '<div id="animated-thumbnails">';
+$dir = './';
+$files = scd($dir);
+foreach ($files as $file) {
+    if (strpos($file, '.jpg') === true || strpos($file, '.mov') === true) {
+        $output .= '<a href="' . $file . '"><img src="thumbs/' . $file . '" /></a>';
+    }
+}
+echo $output . '</div>';
+?>
+        <script type="text/javascript">
+            lightGallery(document.getElementById('animated-thumbnails'), { thumbnail:true });
+        </script>
+
+    </body>
+</html>
